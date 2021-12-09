5285ea18ea0fb2b5c70241008223d8fed3aa9c10
Young <1334761537@qq.com>
2021-12-09 14:03:23 +0800

    express crud commit

--- app.js
+++ app.js
@@ -1,58 +1,104 @@
 const express = require('express');
-const fs = require('fs'); // node 文件系统
+const {getDb, saveDb} = require("./db");
 const app = new express();
 
-/*app.get('/',(req, res) => {
-    res.status(200).send('请求成功！');
-    res.end('Hello World');
-})
-app.post('/123',(req, res) => {
-    res.end('post');
-})
-app.put('/user',(req, res) => {
-    res.send('access /user')
-})
-app.delete('/detail', (req , res) => {
-    res.send('delete detail');
-})*/
+// 设置 request body 解析配置
+app.use(express.json()); // 配置解析json body 用于post
+app.use(express.urlencoded()); // 配置解析x-www-form-urlencoded body 用于post
 
 // request: 前端 => 后端 response：后端 => 前端
 
 // 获取列表
-app.get('/todos',(req, res) => {
+app.get('/todos', async (req, res) => {
 //    一般接口请求后会判断 是否携带token 以及token是否过期 过期以及没有token 会返回403禁止访问
-    fs.readFile('./db.json', 'utf8', (err, data) => {
-        if (err){
-            // 文件读取失败则返回 500 状态
-            return  res.status(500).json({error: err.message})
-        }
-        const val = JSON.parse(data);
-        res.status(200).json(val.todos);
-
-    }); // 读取文件为异步操作
-    // res.status(200).send(todos);
+    try {
+        const data = await getDb();
+        res.status(200).json(data.todos);
+    } catch (err){
+        res.status(500).json({error: err.message})
+    }
 })
 // 获取列表中的某一条数据
-app.get('/todos/:id', (req, res) => {
-    fs.readFile('./db.json', 'utf8', (err, data) => {
-        if (err){
-            // 文件读取失败则返回 500 状态
-            return  res.status(500).json({error: err.message})
-        }
-        // 此处遍历数组查找具体元素
-        const id = Number(req.params.id)
-        const todoItem = JSON.parse(data).todos.find(item => item.id === id);
-        if (todoItem){
-            res.status(200).json(todoItem); // 找到就返回200状态码，并返回查找项
-        }else {
-            res.status(404).end('未找到数据'); // 找不到id 则返回404 并结束响应
-        }
-    }); // 读取文件为异步操作
+app.get('/todos/:id', async (req, res) => {
+    try {
+    const id = Number(req.params.id)
+    const data = await getDb();
+    const todoItem = data.todos.find(item => item.id === id);
+    if (todoItem){
+        res.status(200).json(todoItem); // 找到就返回200状态码，并返回查找项
+    }else {
+        res.status(404).end('未找到数据'); // 找不到id 则返回404 并结束响应
+    }
+    } catch (err){
+        res.status(500).json({error: err.message})
+    }
 })
-
-
+// 列表新增
+app.post('/todos/add',  async (req, res) => {
+  try {
+      const { name } = req.body; // 从请求体里结构name
+      // 此处name有值才赋值，没有直接返回422
+      if (!name){
+          return res.status(422).json({error: "field must fill in"})
+      } else {
+          const data = await getDb();
+          const lastId = data.todos.length + 1;
+          res.end();
+          data.todos.push({
+              id: lastId,
+              name: name
+          })
+          await saveDb(data);
+      }
+  }catch (err){
+      res.status(500).json({error: err.message})
+  }
+})
+// 列表修改
+app.patch('/todos/edit/:id', async (req, res) => {
+    // 1.获取请求路径后的id
+    // 2.获取数据
+    // 2.1 数据校验 有该id则修改 没有则返回422 请求参数有误
+    // 3.修改数据
+    try {
+        const id = Number(req.params.id);
+        const name = req.body.name;
+        if(!name) return res.status(422).json({error: "must return name"});
+        const todos  = await getDb();
+        const idIndex = todos.findIndex(item => item.id === id);
+        if (idIndex !== -1){
+            todos[idIndex].name = req.body.name;
+            await saveDb(todos);
+            res.status(200).end('修改成功');
+        }else {
+            res.status(404).json({error: `没有找到id为${id}的数据`})
+        }
+    }catch (err) {
+        res.status(500).json({error: err.message})
+    }
+})
+// 列表删除
+app.delete('/todos/delete/:id', async (req, res) => {
+    try {
+        const  id  = Number(req.params.id);
+        if (id){
+            const todos = await getDb();
+            const delIndex = todos.findIndex(item => item.id === id);
+            if (delIndex !== -1){
+                todos.splice(delIndex, 1);
+                await saveDb(todos);
+            }else {
+                res.status(422).json({error: `没有id为${id}的数据可删除`})
+            }
+            res.status(200).json(todos);
+        }else {
+            res.status(422).json({error: `id must return `})
+        }
+    }catch (e) {
+        res.status(500).json({error: e.message});
+    }
+})
+// 端口监听
 app.listen(3000, () => {
     console.log('server running localhost 3000');
 })
-
-// req
--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 const fs = require("fs");
-exports.getDb = () => {
-    fs.readFile('./db.json', 'utf8', (err, data) => {
-        if (err){
-            // 文件读取失败则返回 500 状态
-            return  res.status(500).json({error: err.message})
-        }
-        const val = JSON.parse(data);
-        res.status(200).json(val.todos);
+const path = require('path');
 
-    });
+const { promisify } = require('util'); // 引入node 工具库
+const readFile = promisify(fs.readFile); // 读取文件方法转 promise
+const writeFile = promisify(fs.writeFile); // 写入文件方法转 promise
+
+const dbPath = path.join(__dirname, './db.json'); // 路径拼接
+const code = 'utf8';
+exports.getDb = async () => {
+    const data = await readFile(dbPath, code);
+    return JSON.parse(data);
+}
+exports.saveDb = async db => {
+    // 1.首先将db数据转json格式
+    const data = JSON.stringify(db, null, '  ');
+    // 2.然后将数据重新写入db.json文件中
+    await writeFile(dbPath, data);
 }
